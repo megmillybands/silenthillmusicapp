28b60ff36b497dd24a29bc15360d8fb174586d3a
megmillybands <meganlynncramer@gmail.com>
2026-07-23 01:50:21 -0500

    django is being difficult

--- silenthillmusicapp/app/models.py
+++ silenthillmusicapp/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Album(models.Model):
     name = models.CharField(max_length=200)
-    album_cover = models.CharField(max_length=200)
+    album_cover = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.name
